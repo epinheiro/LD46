8dd5becc6f8d20356a78e34a51e371e254a546de
Eduardo Pinheiro <edu.g.pinheiro@gmail.com>
2020-04-18 10:34:51 -0300

    :sparkles::construction: Beginning of the enemy perception system

---
+++ Assets/EnemyPerceptionController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPerceptionController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other){
+        Debug.Log(string.Format("Collision with {0} detected", other.gameObject.name));
+    }
+}
---
+++ Assets/Scripts/PlayerTracker.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTracker : MonoBehaviour
+{
+    public GameObject player;
+
+    bool IsPlayerDetected = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
