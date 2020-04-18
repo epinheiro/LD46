54f5d5840875f25f45f217744769d5bbe1f94aaa
Eduardo Pinheiro <edu.g.pinheiro@gmail.com>
2020-04-18 10:00:13 -0300

    :sparkles: Enemy uses simple Navigator

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,30 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyController : MonoBehaviour
 {
-    [Range(1, 100)] public float moveSpeed = 15f;
+    [Range(1, 100)] public float moveSpeed = 30f;
+    [Range(1, 360)] public float angularSpeed = 180;
 
     // Object hierarchy from ROUTE POINTS
     public RouteController route;
 
+    NavMeshAgent agent;
 
-    
     // Start is called before the first frame update
     void Start()
     {
         route = route.GetComponent<RouteController>();
         route.SetupRoute();
+
+        SetupNavMeshAgent();
     }
 
     // Update is called once per frame
     void Update()
     {
         if ( Mathf.Abs( Vector3.Distance(this.transform.position, route.GetCurrentRoutePoint() ) ) > 0.1f ) {
-            transform.position = Vector3.MoveTowards(this.transform.position, route.GetCurrentRoutePoint(), moveSpeed * Time.deltaTime);
+            agent.destination = route.GetCurrentRoutePoint();
+            // transform.position = Vector3.MoveTowards(this.transform.position, route.GetCurrentRoutePoint(), moveSpeed * Time.deltaTime);
         } else {
             route.NextRoutePoint();
         }
+    }
+
+    void SetupNavMeshAgent(){
+        agent = this.GetComponent<NavMeshAgent>();
+        agent.speed = moveSpeed;
+        agent.angularSpeed = angularSpeed;
     }
 }
