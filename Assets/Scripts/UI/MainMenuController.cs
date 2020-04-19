5cbee18ee5d48082f4f1fddef9ce429778e3583d
Eduardo Pinheiro <edu.g.pinheiro@gmail.com>
2020-04-19 17:11:44 -0300

    :art::sparkles: Fade out effect for Main Menu

--- Assets/Scripts/UI/MainMenuController.cs
+++ Assets/Scripts/UI/MainMenuController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Playables;
+using UnityEngine.UI;
 
 public class MainMenuController : MonoBehaviour
 {
@@ -38,4 +39,69 @@ public class MainMenuController : MonoBehaviour
         Application.Quit();
     }
 
+    void StartFadeOutMenu(){
+        StartCoroutine(FadeOutMenu());
+    }
+
+    void FadeBeginning(){
+        DeactiveButton(this.gameObject);
+    }
+
+    void DeactiveButton(GameObject current){
+        Button button = current.GetComponent<Button>();
+        if(button!=null) button.enabled = false;
+
+        int childCount = current.transform.childCount;
+
+         if(childCount != 0){
+            for( int i=0; i<childCount; i++){
+                DeactiveButton(current.transform.GetChild(i).gameObject);
+            }
+        }
+    }
+
+    void FadeEnding(){
+        Destroy(this.gameObject);
+    }
+
+    IEnumerator FadeOutMenu(float time = .75f){
+        FadeBeginning();
+
+        float hardcodedTimeStep = 0.01f;
+
+        float step = -1/(time/hardcodedTimeStep);
+
+        float timeCount = 0;
+
+        List<CanvasRenderer> canvasList = new List<CanvasRenderer>();
+        ListCanvasRendererRecursive(this.gameObject, canvasList);
+
+        while(timeCount < time){
+            timeCount += hardcodedTimeStep;
+
+            foreach(CanvasRenderer cr in canvasList){
+                float alpha = cr.GetAlpha();
+                cr.SetAlpha(alpha+step);
+            }
+
+            yield return new WaitForSeconds(hardcodedTimeStep);
+        }
+
+        FadeEnding();
+    }
+
+    void ListCanvasRendererRecursive(GameObject current, List<CanvasRenderer> list){
+        CanvasRenderer renderer = current.GetComponent<CanvasRenderer>();
+        if(renderer != null){
+            list.Add(renderer);
+        }
+
+        int childCount = current.transform.childCount;
+
+        if(childCount != 0){
+            for( int i=0; i<childCount; i++){
+                ListCanvasRendererRecursive(current.transform.GetChild(i).gameObject, list);
+            }
+        }
+    }
 }
